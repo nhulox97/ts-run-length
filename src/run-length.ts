4f0eebde4d6054b5ddcb7beff2e0a0396b0505b2
Sergio Bernal <sergiobernal909@gmail.com>
2021-07-02 10:21:58 -0600

    ✨ Add decoder method
    Since the data was split by linebreak, I can do the decoding proccess each line at time, so I use a reduce fucntion to return the decoded line for each map iteration

--- src/run-length.ts
+++ src/run-length.ts
@@ -44,18 +44,29 @@ export class RunLen extends FSRunLen {
     // if input file has not the right format, then throw it
     if (!hasDecodableFormat(this.data)) throw 'The content has not the supported format';
 
-    console.log('Encode');
+    const dataLines = this.dataArray.map((dataLine) => {
+      return dataLine
+        .split('')
+        .reduce(
+          (acc, currChar, idxChar, chars) =>
+            idxChar % 2 === 0
+              ? acc + chars[idxChar + 1].repeat(currChar as unknown as number)
+              : acc,
+          ''
+        );
+    });
+    this.setData(joinDataByLineBreak(dataLines));
+    this.showData();
   }
 
   encode(): void {
     // if input file has not the right format, then throw it
     if (!hasCodableFormat(this.data)) throw 'The content has not the supported format';
 
-    let dataLines = this.dataArray;
     let lineIdx = 0;
     let charCount;
-    let encodedLine = '';
-    dataLines = dataLines.map((dataLine) => {
+    let encodedLine;
+    const dataLines = this.dataArray.map((dataLine) => {
       charCount = 1;
       encodedLine = '';
       for (lineIdx = 1; lineIdx <= dataLine.length; lineIdx += 1) {
